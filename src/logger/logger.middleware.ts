06bb4a36d9445a43f95f8f74436534d7b55b46ee
Abhay <63441094+Abhay2133@users.noreply.github.com>
2025-12-31 18:25:08 +0000

    Refactor LoggerMiddleware to improve timestamp formatting in log output

--- src/logger/logger.middleware.ts
+++ src/logger/logger.middleware.ts
@@ -6,10 +6,10 @@ export class LoggerMiddleware implements NestMiddleware {
   use(req: Request, res: Response, next: NextFunction) {
     const hr = new Date().getHours();
     const min = new Date().getMinutes();
-    const sec = new Date().getSeconds();
     const ms = new Date().getMilliseconds();
+    const sec = new Date().getSeconds() + ms / 1000;
     console.log(
-      `${res.statusCode} ${req.method} ${req.url} ${hr}:${min}:${sec}:${ms}`,
+      `${res.statusCode} ${req.method} ${req.url} ${hr}:${min}:${sec}`,
     );
     next();
   }
