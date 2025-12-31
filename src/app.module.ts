c9f822fc9e840e1fccc161ce63e7ddd7be780106
Abhay <63441094+Abhay2133@users.noreply.github.com>
2025-12-31 18:12:10 +0000

    Add LoggerMiddleware and integrate it into the application

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,16 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AdminModule } from './admin/admin.module';
+import { LoggerMiddleware } from './logger/logger.middleware';
 
 @Module({
   imports: [AdminModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes('*');
+  }
+}
---
+++ src/logger/logger.middleware.ts
@@ -0,0 +1,13 @@
+import { Injectable, NestMiddleware } from '@nestjs/common';
+import { Request, Response, NextFunction } from 'express';
+
+@Injectable()
+export class LoggerMiddleware implements NestMiddleware {
+  use(req: Request, res: Response, next: NextFunction) {
+    const hr = new Date().getHours();
+    const min = new Date().getMinutes();
+    const sec = new Date().getSeconds();
+    console.log(`${res.statusCode} ${req.url} ${hr}:${min}:${sec}`);
+    next();
+  }
+}
